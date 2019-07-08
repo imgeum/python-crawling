89948af0366e721a21600521f62e1b42588f6236
Dokyun Lim <idk255@naver.com>
2019-07-08 11:27:26 +0900

    [Add] 190702_crawl files

--- 190702_crawl/main.py
+++ 190702_crawl/main.py
@@ -8,15 +8,8 @@ def get_html(url):
         _html = resp.text
     return _html
 
-url = "https://comic.naver.com/webtoon/detail.nhn?titleId=703852&no=68&weekday=tue"
-cmt_url = '/comment/comment.nhn?titleId=703852&no=68'
-html = get_html(url + cmt_url)
+# iframe 'comment' src
+url = 'https://comic.naver.com/comment/comment.nhn?titleId=703852&no=68&weekday=tue'
+html = get_html(url)
 soup = BeautifulSoup(html, 'html5lib')
-soup = BeautifulSoup(html, 'html.parser')
-comment = soup.find("ul", {"class":"img_list"}
-        ).find_all("div", {"class":"thumb"})
 
-for webtoon in webtoons:
-    info_soup = webtoon.find("img")
-    _title = info_soup["title"]
-    print(_title)

ab2ba42296ac7bdfc33d913b5a30411b1f731344
Dokyun Lim <idk255@naver.com>
2019-07-09 09:13:34 +0900

    commit for another v

--- naver-movie-comment/comment-analsys/main.R
+++ naver-movie-comment/comment-analsys/main.R
@@ -4,11 +4,14 @@
 
 
 library(tidyverse)
+
 library(rJava)
 library(wordcloud2)
 library(RColorBrewer)
 library(KoNLP)
 library(stringr)
+library(dplyr)
+
 library(data.table)
 library(reshape2)
 library(data.table)
@@ -17,44 +20,21 @@ useSejongDic()
 # useNIADic()
 
 # csv to str
-
-Sys.setlocale("LC_ALL", "C")
-comment_datas <- read.csv("data/movie_comment.csv", header = T, encoding = "UTF-8", nrows=200)
+comment_datas <- read.csv("data/movie_comment.csv", header = T, encoding = "UTF-8", nrows=4000)
 comment_df <- comment_datas %>% select('cmt')
 
 comment_str <- toString(comment_df)
 comment_list <- as.list(comment_df)
-comment_str <- paste(unlist(comment_list), collapse='')
+# head(comment_list$cmt)
+comment_str <- paste(unlist(comment_list$cmt), collapse='')
 
 # 
 words <- sapply(comment_str, extractNoun, USE.NAMES = F)
 words_filtered <- Filter(function(x) {nchar(x) <= 15}, words)
-words_filtered
 
-# write.table(unlist(words_filtered), "data/comment_aft_proc.txt", fileEncoding="UTF-8")
-# re_words <- fread("data/comment_aft_proc.txt", encoding="UTF-8")
-# 
-# wordcount <- table(re_words)
-# head(sort(wordcount, decreasing=T), 30)
+table_words <- table(words_filtered)
 
-
-
-
-
-SimplePos09("최대한 친절하게 쓴 R")
-
-
-
-
-
-
-
-
-
-
-
-
-
+# SimplePos09("최대한 친절하게 쓴 R")
 
 # nrow(data4)
 # wordcount <- table(data4)
---
+++ naver-movie-comment/help.md
@@ -0,0 +1,6 @@
+https://kuduz.tistory.com/1090
+
+우선 r-lecture의 소스를 따라하고
+왜 파일 저장 전후가 다르지? encoding
+그 다음 인터넷 예제 따라하기
+
